4b2532d25cb5e7f1004804534fbca2fa1a26a321
elisabetta-cappa <100422061+elisabetta-cappa@users.noreply.github.com>
2025-04-22 09:30:03 +0000

    Deploying to gh-pages from @ elisabetta-cappa/elisabetta-cappa.github.io@4f9f1e0c7eceebccfc530995bcc359e5de463a55 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,14 +501,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%65%6C%69%73%61%62%65%74%74%61.%63%61%70%70%61@%73%61%6E%74%61%6E%6E%61%70%69%73%61.%69%74", "_blank");
         },
       },{
         id: 'social-rss',
@@ -522,14 +515,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=RudXyqcAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
